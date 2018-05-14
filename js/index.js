f22ba69e614ec6a6922ae0e6994e112bbc08d7f4
weiiipf <1098419929@qq.com>
2018-05-14 22:51:34 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var $wrapper = $('.wrapper');
+
+var timer = setTimeout(function () {
+    $wrapper.removeClass('init');
+}, 200);
+
+$('.item').on('click', function () {
+    $(this).addClass('active');
+    $wrapper.addClass('wrapper-active');
+})
+$('.close').on('click', function (e) {
+    e.stopPropagation();
+    $('.active').removeClass('active');
+    $wrapper.removeClass('wrapper-active');
+})
